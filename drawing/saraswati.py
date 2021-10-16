efc1a5d0b2959a69e81ef1a333dcc04c33524c69
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2021-10-16 15:11:58 +0530

    Update saraswati.py

--- drawing/saraswati.py
+++ drawing/saraswati.py
@@ -1,4 +1,9 @@
-import turtle
+import turtle, pygame
+
+pygame.init()
+
+pygame.mixer.music.load('sound/saraswati-bgsound.mp3')
+pygame.mixer.music.play(-1)
 
 t = turtle.Turtle()
 t.getscreen().bgcolor('black')
@@ -11,7 +16,6 @@ t.left(60)
 t.forward(550)
 
 def hand(i):
-	
 	t.pendown()
 	t.circle(15)
 	t.backward(550-i)
@@ -19,21 +23,19 @@ def hand(i):
 	t.forward(100+i)
 	t.left(120)
 	t.forward(500-i)
-	
+  
 	t.right(120)
 	t.penup()
 	t.circle(50,extent=180)
 	t.pendown()
 	t.circle(50,extent=180)
-	
+  
 	t.penup()
 	t.forward(300-(i*2))
 	t.left(60)
-	
+  
 	t.forward(50 if i < 400 else 600)
-
+  
 if __name__ == "__main__" :
 	for i in range(0,500,100):
 		hand(i)
-		
-	turtle.done()
